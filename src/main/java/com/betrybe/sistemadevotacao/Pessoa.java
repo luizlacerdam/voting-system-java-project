510e2a739f6a8538ac5c7824387408ec26df1684
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-12 15:31:48 -0300

    :sparkles: [feat] Pessoa-done

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,19 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * Classe abstrata Pessoa.
+ */
+public abstract class Pessoa {
+  /**
+   * Atributos e métodos.
+   */
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
