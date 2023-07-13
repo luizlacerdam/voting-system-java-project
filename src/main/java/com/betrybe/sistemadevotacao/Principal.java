92134658b94e1775e01e76b800e98c10ac07802f
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-13 20:42:14 -0300

    :sparkles: [feat] Principal-cadastroPessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -13,21 +13,24 @@ public class Principal {
   public static void main(String[] args) {
     GerenciamentoVotacao gerenciamentoVotacao = new GerenciamentoVotacao();
 
-
     Scanner scanner = new Scanner(System.in);
+    int opcao = 0;
+
+    while(opcao != 2) {
     System.out.println("Cadastrar pessoa candidata?\n"
         + "1 - Sim\n"
         + "2 - Não\n"
         + "Entre com o número correspondente à opção desejada:");
 
-    String opcao = scanner.nextLine();
-
-    if (opcao == "1") {
+    String opcaoString = scanner.nextLine();
+    opcao = Integer.parseInt(opcaoString);
+    if (opcao == 1) {
       System.out.println("Entre com o nome da pessoa candidata:");
       String nomeCandidata = scanner.nextLine();
       System.out.println("Entre com o número da pessoa candidata:");
       String numeroCadidata = scanner.nextLine();
       gerenciamentoVotacao.cadastrarPessoaCandidata(nomeCandidata, Integer.parseInt(numeroCadidata));
+    }
     }
 
   }
