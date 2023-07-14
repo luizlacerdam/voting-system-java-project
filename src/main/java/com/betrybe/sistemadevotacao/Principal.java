64a2425f3a9b49538c49d7f349bfcb1800c6241c
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-14 09:57:46 -0300

    :sparkles: [feat] Principal-cadastroPessoaCandidata-como-metodo

--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -3,36 +3,38 @@ package com.betrybe.sistemadevotacao;
 import java.util.Scanner;
 
 /**
- * Classe principal com menu de interação.
+ * Classe principal com menu.
  */
 public class Principal {
 
   /**
-   * Construtor, atribuindo as opções internas do menu comum.
+   * Classe main, constructor.
    */
   public static void main(String[] args) {
     GerenciamentoVotacao gerenciamentoVotacao = new GerenciamentoVotacao();
-
     Scanner scanner = new Scanner(System.in);
+    Principal.cadastrarCandidato(scanner, gerenciamentoVotacao);
+  }
+
+  public static void cadastrarCandidato(Scanner scanner, GerenciamentoVotacao gerenciamentoVotacao) {
     int opcao = 0;
 
     while(opcao != 2) {
-    System.out.println("Cadastrar pessoa candidata?\n"
-        + "1 - Sim\n"
-        + "2 - Não\n"
-        + "Entre com o número correspondente à opção desejada:");
+      System.out.println("Cadastrar pessoa candidata?\n"
+          + "1 - Sim\n"
+          + "2 - Não\n"
+          + "Entre com o número correspondente à opção desejada:");
 
-    String opcaoString = scanner.nextLine();
-    opcao = Integer.parseInt(opcaoString);
-    if (opcao == 1) {
-      System.out.println("Entre com o nome da pessoa candidata:");
-      String nomeCandidata = scanner.nextLine();
-      System.out.println("Entre com o número da pessoa candidata:");
-      String numeroCadidata = scanner.nextLine();
-      gerenciamentoVotacao.cadastrarPessoaCandidata(nomeCandidata, Integer.parseInt(numeroCadidata));
+      String opcaoString = scanner.nextLine();
+      opcao = Integer.parseInt(opcaoString);
+      if (opcao == 1) {
+        System.out.println("Entre com o nome da pessoa candidata:");
+        String nomeCandidata = scanner.nextLine();
+        System.out.println("Entre com o número da pessoa candidata:");
+        String numeroCadidata = scanner.nextLine();
+        gerenciamentoVotacao.cadastrarPessoaCandidata(nomeCandidata, Integer.parseInt(numeroCadidata));
+      }
     }
-    }
-
   }
 
 }
